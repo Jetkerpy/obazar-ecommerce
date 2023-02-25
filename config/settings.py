973980c8228a8a9fd6a122e23edb1f0783f8dda0
berdibaevich <berdibaevichjetker@gmail.com>
2023-02-25 07:23:55 +0500

    jetkerpy

--- config/settings.py
+++ config/settings.py
@@ -133,9 +133,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
