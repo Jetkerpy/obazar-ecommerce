99ab9e26acca227d41f7d1c7f93f23f43ff78a48
berdibaevich <berdibaevichjetker@gmail.com>
2023-02-25 07:00:11 +0500

    jetkerpy

--- config/settings.py
+++ config/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         "NAME": "obazar",
         "USER": "postgres",
         "PASSWORD": "admin",
-        "HOST": '127.0.0.1',
-        "PORT": '5432',
+        "HOST": "127.0.0.1",
+        "PORT": "127.0.0.1",
     }
 }
 
