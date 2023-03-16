e908f3e9657df48332b0770f6a6326e67241e267
berdibaevich <berdibaevichjetker@gmail.com>
2023-03-17 00:41:58 +0500

    edited

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
